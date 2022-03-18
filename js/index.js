082226ff882e0edd2dc035a4c491c93b8e6815dc
Kostas Christou <christou@exeo.gr>
2022-03-18 17:31:31 +0200

    menu added

--- js/index.js
+++ js/index.js
@@ -1,33 +1,61 @@
-const videoOverlayBtn = document.querySelector('.video__play-btn-container');
-
 // Hamburger menu listener
 const hamMenu = document.querySelector('.header__ham-menu');
+const fullMenuLeft = document.querySelector('.full-menu--left');
+const fullMenuRight = document.querySelector('.full-menu--right');
+
 hamMenu.addEventListener('click', () => {
   if (hamMenu.classList.contains('header__ham-menu--open')) {
     hamMenu.classList.remove('header__ham-menu--open');
     hamMenu.classList.add('header__ham-menu--close');
+    fullMenuLeft.classList.remove('full-menu--collapse');
+    fullMenuRight.classList.remove('full-menu--collapse');
   } else {
     hamMenu.classList.remove('header__ham-menu--close');
     hamMenu.classList.add('header__ham-menu--open');
+    fullMenuLeft.classList.add('full-menu--collapse');
+    fullMenuRight.classList.add('full-menu--collapse');
   }
 });
 
-// Hide video overlay on click
-if (videoOverlayBtn) {
-  videoOverlayBtn.addEventListener('click', () => {
-    const videoOverlay = document.querySelector('.video__overlay');
-    videoOverlay.style.zIndex = '-1';
-  });
-}
-
-// Back to top functionallity
+// Back-to-top button functionallity
 const backToTop = document.querySelector('.back-to-top');
 
+function checkPosition() {
+  let windowY = window.scrollY;
+  let screenHeight = screen.height;
+
+  if (windowY < screenHeight) {
+    backToTop.classList.add('hide');
+  } else {
+    backToTop.classList.remove('hide');
+  }
+}
+
 if (backToTop) {
+  window.addEventListener('scroll', checkPosition);
+
   backToTop.addEventListener('click', () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   });
+}
+
+// Hide video overlay on click and show youtube video
+const videoOverlayButtons = document.querySelectorAll(
+  '.video__play-btn-container'
+);
+if (videoOverlayButtons.length > 0) {
+  videoOverlayButtons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      showVideo(e);
+    });
+  });
+}
+
+function showVideo(e) {
+  const videoOverlay = e.target.parentElement;
+
+  videoOverlay.style.display = 'none';
 }
 
 // Calendar functionallity
@@ -37,6 +65,10 @@ if (document.getElementById('color-calendar')) {
     calendarSize: 'small',
   });
 }
+
+//// Playing with calendar
+const calendarDay = document.activeElement('calendar__day-selected');
+console.log(calendarDay);
 
 // Swiper (slider) config
 const meetingsSwiper = new Swiper('.meetings__swiper', {

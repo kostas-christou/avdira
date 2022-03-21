da7f68f68101bd6a1071f0965d2ee11b2a18f73b
Kostas Christou <christou@exeo.gr>
2022-03-21 15:25:41 +0200

    functionality for closing menus when clicking outside added

--- js/index.js
+++ js/index.js
@@ -3,17 +3,50 @@ const hamMenu = document.querySelector('.header__ham-menu');
 const fullMenuLeft = document.querySelector('.full-menu--left');
 const fullMenuRight = document.querySelector('.full-menu--right');
 
-hamMenu.addEventListener('click', () => {
+hamMenu.addEventListener('click', (e) => {
+  e.stopPropagation();
+
   if (hamMenu.classList.contains('header__ham-menu--open')) {
     hamMenu.classList.remove('header__ham-menu--open');
     hamMenu.classList.add('header__ham-menu--close');
     fullMenuLeft.classList.remove('full-menu--collapse');
+    fullMenuLeft.classList.add('full-menu--expand');
     fullMenuRight.classList.remove('full-menu--collapse');
+    fullMenuRight.classList.add('full-menu--expand');
   } else {
     hamMenu.classList.remove('header__ham-menu--close');
     hamMenu.classList.add('header__ham-menu--open');
     fullMenuLeft.classList.add('full-menu--collapse');
+    fullMenuLeft.classList.remove('full-menu--expand');
     fullMenuRight.classList.add('full-menu--collapse');
+    fullMenuRight.classList.remove('full-menu--expand');
+  }
+});
+
+// Close all open menus when clicking outside
+document.addEventListener('click', (e) => {
+  const menusOpen = document.querySelectorAll('.full-menu--expand');
+
+  if (menusOpen.length > 0) {
+    console.log('Menus open!');
+    let menuRemainOpen = [];
+
+    menusOpen.forEach((menu) => {
+      if (menu.contains(e.target)) {
+        menuRemainOpen.push('true');
+      } else {
+        menuRemainOpen.push('false');
+      }
+    });
+
+    if (menuRemainOpen.indexOf('true') === -1) {
+      hamMenu.classList.remove('header__ham-menu--close');
+      hamMenu.classList.add('header__ham-menu--open');
+      fullMenuLeft.classList.remove('full-menu--expand');
+      fullMenuLeft.classList.add('full-menu--collapse');
+      fullMenuRight.classList.remove('full-menu--expand');
+      fullMenuRight.classList.add('full-menu--collapse');
+    }
   }
 });
 
@@ -67,7 +100,6 @@ if (document.getElementById('color-calendar')) {
 }
 
 //// Playing with calendar
-
 const calendarBody = document.querySelector('.calendar__body');
 calendarBody.addEventListener('click', (e) => {
   const calendarDay = document.querySelector('.calendar__day-selected');
@@ -83,9 +115,23 @@ calendarBody.addEventListener('click', (e) => {
 
 // Swiper (slider) config
 const meetingsSwiper = new Swiper('.meetings__swiper', {
-  slidesPerView: 1.4,
-  spaceBetween: 60,
+  slidesPerView: 1,
+  spaceBetween: 20,
   grabCursor: true,
+  breakpoints: {
+    1000: {
+      slidesPerView: 1.4,
+      spaceBetween: 60,
+    },
+    1350: {
+      slidesPerView: 1.4,
+      spaceBetween: 60,
+    },
+    1850: {
+      slidesPerView: 1.4,
+      spaceBetween: 60,
+    },
+  },
   navigation: {
     nextEl: '.meetings__swiper-button-next',
     prevEl: '.meetings__swiper-button-prev',
@@ -99,5 +145,29 @@ const quickAccessSwiper = new Swiper('.quick-access__swiper', {
   navigation: {
     nextEl: '.quick-access__swiper-button-next',
     prevEl: '.quick-access__swiper-button-prev',
+  },
+});
+
+const civilProtectionSwiper = new Swiper('.civil-protection__swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  breakpoints: {
+    850: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    1350: {
+      slidesPerView: 2.5,
+      spaceBetween: 40,
+    },
+    1850: {
+      slidesPerView: 3.5,
+      spaceBetween: 50,
+    },
+  },
+  navigation: {
+    nextEl: '.civil-protection__swiper-button-next',
+    prevEl: '.civil-protection__swiper-button-prev',
   },
 });
